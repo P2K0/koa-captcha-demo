00ede084c08184341cfa0469ad9d9af42475dc4d
P2K0 <3364148324@qq.com>
2023-08-20 16:25:58 +0800

    feat: 完成首页渲染

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,17 +1,27 @@
+import { join } from "node:path";
+
 function randomText(size = 4): string {
   const START_INDEX = 2;
 
   return Math.random().toString(36).slice(START_INDEX, size + START_INDEX);
 }
 
-function randomHexColor():string {
-  const randomComponent = () => Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
+function randomHexColor(): string {
+  const randomComponent = () => Math.floor(Math.random() * 256).toString(16).padStart(2, "0");
 
-  const rHex:string = randomComponent();
-  const gHex:string = randomComponent();
-  const bHex:string = randomComponent();
+  const rHex: string = randomComponent();
+  const gHex: string = randomComponent();
+  const bHex: string = randomComponent();
 
   return `#${rHex}${gHex}${bHex}`;
 }
 
-export { randomText ,randomHexColor};
+function resolvePath(_path: string): string {
+  return join(__dirname, "../", _path);
+}
+
+export {
+  randomText,
+  randomHexColor,
+  resolvePath
+};
